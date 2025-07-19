092f72fd1104c72b8d7780dcc472840cab94e4cb
Paulo Wilson Montori da Rocha <paulo.montori@gmail.com>
2025-07-19 13:16:25 -0300

    Package version adjustments

--- src/PWorx.MicroserviceBoilerPlate/Program.cs
+++ src/PWorx.MicroserviceBoilerPlate/Program.cs
@@ -3,6 +3,12 @@ using OpenTelemetry.Trace;
 using OpenTelemetry.Resources;
 using Serilog;
 using MediatR;
+using PWorx.MicroserviceBoilerPlate.OrderService.Domain.Brokers;
+using PWorx.MicroserviceBoilerPlate.OrderService.Infrastructure.Data;
+using PWorx.MicroserviceBoilerPlate.OrderService.Infrastructure.ViewData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+using PWorx.MicroserviceBoilerPlate.OrderService.Infrastructure.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +18,6 @@ builder.Host.UseSerilog((context, services, configuration) =>
         .Enrich.FromLogContext()
         .WriteTo.Console());
 
-using PWorx.MicroserviceBoilerPlate.OrderService.Domain.Brokers;
-using PWorx.MicroserviceBoilerPlate.OrderService.Infrastructure.Data;
-using PWorx.MicroserviceBoilerPlate.OrderService.Infrastructure.ViewData;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Caching.StackExchangeRedis;
-using PWorx.MicroserviceBoilerPlate.OrderService.Infrastructure.Middleware;
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -70,28 +70,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
